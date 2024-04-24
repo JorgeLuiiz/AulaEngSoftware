ad23ac701cf45f22ecce46ef330d008962727904
jorge.silva <jorge.silva@dcx.ufpb.br>
2024-04-24 09:56:09 -0300

    Commit 2

---
+++ src/DadosCalculadora/Jorge.java
@@ -0,0 +1,19 @@
+package DadosCalculadora;
+
+public class Jorge {
+    private int valor1;
+    private int valor2;
+    private int soma;
+
+    public Jorge(int valor1, int valor2, int soma) {
+        this.valor1 = valor1;
+        this.valor2 = valor2;
+        this.soma = soma;
+    }
+
+    public int calcular(int valor1, int valor2){
+        int resultado = valor1 + valor2;
+        return this.soma = resultado;
+    }
+
+}
