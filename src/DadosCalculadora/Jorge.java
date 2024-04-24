01b16042e59acb70782c745c5e50b353955273b1
Mannoella <mannoellarenata@gmail.com>
2024-04-24 19:12:18 -0300

    alterado

--- src/DadosCalculadora/Jorge.java
+++ src/DadosCalculadora/Jorge.java
@@ -3,17 +3,20 @@ package DadosCalculadora;
 public class Jorge {
     private int valor1;
     private int valor2;
-    private int soma;
+    private int calcular;
 
-    public Jorge(int valor1, int valor2, int soma) {
+    public Jorge(int valor1, int valor2, int calcular) {
         this.valor1 = valor1;
         this.valor2 = valor2;
-        this.soma = soma;
+        this.calcular = calcular;
     }
 
-    public int calcular(int valor1, int valor2){
+    public int calcular(int valor1, int valor2) {
         int resultado = valor1 + valor2;
-        return this.soma = resultado;
+        return this.calcular = resultado;
     }
-
+    public int subtracao(int valor1, int valor2) {
+        int resultado = valor1 - valor2;
+        return this.calcular = resultado;
+    }
 }
---
+++ src/DadosCalculadora/ManuzinhaTest.java
@@ -0,0 +1,18 @@
+package DadosCalculadora;
+
+public class ManuzinhaTest {
+    private int num1;
+    private int num2;
+    private int subtracao;
+
+    public ManuzinhaTest(int num1, int num2, int subtracao) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.subtracao = subtracao;
+    }
+    public int calcular(int num1, int num2) {
+        int resultado = num1 - num2;
+        return this.subtracao = resultado;
+    }
+}
+
