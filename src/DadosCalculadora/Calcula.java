1321a7576bf3be211d367aa7e98832bf02676093
jorge.silva <jorge.silva@dcx.ufpb.br>
2024-04-24 09:39:02 -0300

    Testando Commit

---
+++ src/DadosCalculadora/Calcula.java
@@ -0,0 +1,14 @@
+package DadosCalculadora;
+
+public class Calcula {
+    static int valor1;
+    static int valor2;
+    static int soma;
+
+
+    public Calcula()
+    {
+        soma = valor1 + valor2;
+
+    }
+}
